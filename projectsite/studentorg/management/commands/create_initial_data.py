836069f5e1a3c11669887115d35e7173f148fa50
joshISIP <202080065@psu.palawan.edu.ph>
2025-03-06 15:57:49 +0800

    loaded initial data

---
+++ projectsite/studentorg/management/commands/create_initial_data.py
@@ -0,0 +1,48 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from studentorg.models import College, Program, Organization, Student, OrgMember    
+
+class Command(BaseCommand):
+    help = "Create initial data for studentorg app"
+
+    def handle(self, *args, **kwargs):
+        self.fake = Faker()
+        self.fake_ph = Faker('en_PH')
+        self.create_organization(10)
+        self.create_students(50)
+        self.create_membership(10)
+
+    def create_organization(self, count):
+        for _ in range(count):
+            words = [self.fake.word() for _ in range(2)]
+            organization_name = " ".join(words)
+            Organization.objects.create(
+                name=organization_name.title(),
+                college=College.objects.order_by('?').first(),
+                description=self.fake.sentence()
+            )
+        self.stdout.write(self.style.SUCCESS('Organizations created successfully'))
+        
+    def create_students(self, count):
+        for _ in range(count):
+            program = Program.objects.order_by('?').first()
+            if program is not None:
+                Student.objects.create(
+                    student_id=f"{self.fake_ph.random_int(2020,2024)}-{self.fake_ph.random_int(1,8)}-{self.fake_ph.random_number(digits=4)}",
+                    lastname=self.fake_ph.last_name(),
+                    frstname=self.fake_ph.first_name(),
+                    middlename=self.fake_ph.first_name(),
+                    program=program
+                )
+        self.stdout.write(self.style.SUCCESS('Students created successfully'))
+    
+    def create_membership(self, count):
+        for _ in range(count):
+            student = Student.objects.order_by('?').first()
+            organization = Organization.objects.order_by('?').first()
+            if student and organization:
+                OrgMember.objects.create(
+                    student=student,
+                    organization=organization
+                )
+        self.stdout.write(self.style.SUCCESS('Memberships created successfully'))
